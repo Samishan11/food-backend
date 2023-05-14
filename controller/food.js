a8b3f75d158d2ea4587be35912e5cf1819994586
Samishan11 <samishanthapa0@gmail.com>
2023-05-14 13:54:29 +0545

    changes

--- controller/food.js
+++ controller/food.js
@@ -1,72 +1,83 @@
-const FoodModel = require('../model/foodModel')
+const FoodModel = require("../model/foodModel");
 
 // add food
 exports.addfood = async (req, res) => {
-    try {
-        const Food = await new FoodModel({
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.description,
-            catagory: req.body.catagory,
-            image: req.file?.path
-        })
-        Food.save()
-        res.json({ 'Foods': Food })
-    } catch (error) {
-        res.json(error)
-    }
-}
+  console.log("first");
+  try {
+    const Food = new FoodModel({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      catagory: req.body.catagory,
+      image: req?.file?.path,
+    });
+    await Food.save();
+    res.json({ Foods: Food });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+};
 
-// update food 
+// update food
 exports.updatefood = async (req, res) => {
-    try {
-        const Foodupdate = await FoodModel.findOneAndUpdate({ _id: req.params.foodid }, {
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.description,
-            catagory: req.body.catagory,
-            image: req.file?.path
-        },
-            { new: true }
-        )
-        res.json(Foodupdate)
-    } catch (error) {
-        res.json(error)
-    }
-}
+  try {
+    const Foodupdate = await FoodModel.findOneAndUpdate(
+      { _id: req.params.foodid },
+      {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        catagory: req.body.catagory,
+        image: req.file?.path,
+      },
+      { new: true }
+    );
+    res.json(Foodupdate);
+  } catch (error) {
+    res.json(error);
+  }
+};
 
-// star rating food 
+// star rating food
 exports.starRating = async (req, res) => {
-
-    const food_ = await FoodModel.findById(req.params.foodid)
-    var totalrating = food_.rating?.reduce((sum, curr) => sum + Number(curr.rating ? curr.rating : 0), 0)
-    const user = req.body.user
-    var filter = food_.rating.find(val => val.user === user)
-    let abc;
-    food_.rating.filter(val => {
-        if (val.user === user) {
-            abc = val.rating
-        }
-    })
-    if (!filter) {
-        food_.totalrating = (totalrating + req.body.rating) / (food_.rating.length >= 1 ? food_.rating.length + 1 : 1)
-        const Foodupdate = await FoodModel.findOneAndUpdate({ _id: req.params.foodid }, {
-            $push: {
-                rating: req.body
-            }
-        }
-        )
-       return res.json(Foodupdate)
-    } else {
-        for (var i in food_.rating) {
-            if (food_.rating[i].user === user) {
-                food_.rating[i] = req.body
-                food_.totalrating = (totalrating - abc + req.body.rating) / (food_.rating.length)
-            }
-        }
+  const food_ = await FoodModel.findById(req.params.foodid);
+  var totalrating = food_.rating?.reduce(
+    (sum, curr) => sum + Number(curr.rating ? curr.rating : 0),
+    0
+  );
+  const user = req.body.user;
+  var filter = food_.rating.find((val) => val.user === user);
+  let abc;
+  food_.rating.filter((val) => {
+    if (val.user === user) {
+      abc = val.rating;
     }
-    food_.save()
-}
+  });
+  if (!filter) {
+    food_.totalrating =
+      (totalrating + req.body.rating) /
+      (food_.rating.length >= 1 ? food_.rating.length + 1 : 1);
+    const Foodupdate = await FoodModel.findOneAndUpdate(
+      { _id: req.params.foodid },
+      {
+        $push: {
+          rating: req.body,
+        },
+      }
+    );
+    return res.json(Foodupdate);
+  } else {
+    for (var i in food_.rating) {
+      if (food_.rating[i].user === user) {
+        food_.rating[i] = req.body;
+        food_.totalrating =
+          (totalrating - abc + req.body.rating) / food_.rating.length;
+      }
+    }
+  }
+  food_.save();
+};
 // exports.starRating = async (req, res) => {
 
 //     const food_ = await FoodModel.findById(req.params.foodid)
@@ -97,32 +108,32 @@ exports.starRating = async (req, res) => {
 
 //  get all foods
 exports.getfood = async (req, res) => {
-    try {
-        let Food;
-        const name = req.query.name;
-        const catagory = req.query.catagory;
-        if (name) {
-            Food = await FoodModel.find({ name: name })
-        } else if (catagory) {
-            Food = await FoodModel.find({
-                catagory: {
-                    $in: [catagory]
-                }
-            })
-        } else {
-            Food = await FoodModel.find()
-        }
-        res.json(Food)
-    } catch (error) {
-        res.json(error)
+  try {
+    let Food;
+    const name = req.query.name;
+    const catagory = req.query.catagory;
+    if (name) {
+      Food = await FoodModel.find({ name: name });
+    } else if (catagory) {
+      Food = await FoodModel.find({
+        catagory: {
+          $in: [catagory],
+        },
+      });
+    } else {
+      Food = await FoodModel.find();
     }
-}
+    res.json(Food);
+  } catch (error) {
+    res.json(error);
+  }
+};
 //  food detail
 exports.fooddetail = async (req, res) => {
-    try {
-        const Food = await FoodModel.findOne({ _id: req.params.food })
-        res.json(Food)
-    } catch (error) {
-        res.json(error)
-    }
-}
+  try {
+    const Food = await FoodModel.findOne({ _id: req.params.food });
+    res.json(Food);
+  } catch (error) {
+    res.json(error);
+  }
+};
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const auth  = require('../middleware/admin')
-const upload = require('../upload/upload')
-const {addfood,updatefood,getfood ,fooddetail, starRating} = require('../controller/food')
+const auth = require("../middleware/admin");
+const upload = require("../upload/upload");
+const {
+  addfood,
+  updatefood,
+  getfood,
+  fooddetail,
+  starRating,
+} = require("../controller/food");
 
-router.post('/addfood', upload.single('image') , addfood)
-router.put('/updatefood/:foodid', upload.single('image') , updatefood)
-router.put('/rating-rating/:foodid', starRating)
-router.get('/getallfood', getfood)
-router.get('/fooddetail/:food', fooddetail)
+router.post("/addfood", upload.single("image"), addfood);
+router.put("/updatefood/:foodid", upload.single("image"), updatefood);
+router.put("/rating-rating/:foodid", starRating);
+router.get("/getallfood", getfood);
+router.get("/fooddetail/:food", fooddetail);
 
 module.exports = router;
