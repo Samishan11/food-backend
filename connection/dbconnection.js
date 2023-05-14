739bf5a5095d0a382dece0254c4e7d171d16a747
Samishan11 <samishanthapa0@gmail.com>
2023-05-14 12:50:07 +0545

    changes

--- connection/dbconnection.js
+++ connection/dbconnection.js
@@ -1,7 +1,10 @@
-const mongoose =require('mongoose');
-mongoose.connect('mongodb+srv://jokershan:password123joker@cluster0.cyu7q.mongodb.net/mernfoodapp?retryWrites=true&w=majority',{
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.db, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("connected to mongodb");
-}).catch()
+  })
+  .catch();
--- utils/mail.js
+++ utils/mail.js
@@ -1,11 +1,11 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 exports.mail = () =>
-    nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.HOST,
-            pass: process.env.PASS
-        }
-    });
+  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.HOST,
+      pass: process.env.PASS,
+    },
+  });
